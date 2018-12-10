4a51a5cf97842ab4687ea62f344466d8f3dd0392
WolfyMC123HD <45721775+WolfyMC123HD@users.noreply.github.com>
2018-12-09 21:10:49 -0500

    Add files via upload

---
+++ fun.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+class Fun:
+	"""Repeats text"""
+	
+	def __init__(self, client):
+		self.client = client
+	
+	@commands.command(no_pm=True)
+	async def mimic(self, *, text):
+		"""Copies your words"""
+		await self.client.say(text)
+		
+		
+def setup(client):
+	client.add_cog(Fun(client))
